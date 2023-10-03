d88c18f850edba69f2e4c4c9b8af4ebb0169345e
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-10-02 22:03:16 -0400

    get parameters from supp mat

--- lib/parameters.jl
+++ lib/parameters.jl
@@ -1,11 +1,11 @@
-S = 20 # Number of species
+S = 25 # Number of species
 timesteps = 500 # Timesteps for the simulation
 A = zeros(Float64, (4, S, S)) # Interactions -- transmission, mutualism, competition, predation
 Co = 0.5 # Connectance of the matrix
 K = 100.0 # Carrying capacity
-σₓ = 0.02 # Dispersal for biotic interactions
-σᵢ = 0.01 # Dispersal for disease transmission
-δ = 0.02 # Self-attenunation parameter for infection
+σₓ = 0.2 # Dispersal for biotic interactions
+σᵢ = 0.001 # Dispersal for disease transmission
+δ = 0.02 # Self-regulation parameter for infection
 r = ones(S) # Growth rate
 ν = 0.05ones(S) # Virulence
 ρ = 0.05ones(S) # Recovery
