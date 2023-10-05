25b5366ea8fb7d9058bb5c7137a0bd5ceb9c96e7
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-10-05 10:07:06 -0400

    ignore sets

--- 00_generate_simulation_parameters.jl
+++ 00_generate_simulation_parameters.jl
@@ -17,7 +17,7 @@ if ~ispath("slurm")
 end
 
 bank = DataFrame()
-centers = collect(0:20:100)
+centers = collect(0:5:100)
 barycenters = [(c1, c2, c3) for c1 in centers for c2 in centers for c3 in centers]
 filter!(b -> isequal(100)(sum(b)), barycenters)
 
@@ -31,7 +31,7 @@ for b in barycenters
             competition=b[2],
             predation=b[3],
             density=density,
-            replicates=10,
+            replicates=1000,
             richness=20,
             time=500,
             connectance=0.5,
@@ -51,8 +51,7 @@ CSV.write(joinpath(input_path, "sets.csv"), bank)
 # Write the SLURM file to go with the job
 job_file = """
 #! /bin/bash
-#SBATCH --time=
-#SBATCH --array=1-$(size(bank, 1))%250
+#SBATCH --array=1-$(size(bank, 1))
 #SBATCH --time=02:00:00
 #SBATCH --mem-per-cpu=2500M
 #SBATCH --cpus-per-task=64
@@ -62,3 +61,4 @@ job_file = """
 module load StdEnv/2020 julia/1.9.1
 julia --project -t 64 01_run_one_parameter.jl
 """
+write("dilution.sh", job_file)
---
+++ dilution.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#SBATCH --array=1-462
+#SBATCH --time=02:00:00
+#SBATCH --mem-per-cpu=2500M
+#SBATCH --cpus-per-task=64
+#SBATCH --job-name=disease-dilution-effect
+#SBATCH --output=slurm/%x-%a.out
+
+module load StdEnv/2020 julia/1.9.1
+julia --project -t 64 01_run_one_parameter.jl
